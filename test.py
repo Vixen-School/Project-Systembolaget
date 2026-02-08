ece88cc1f911ac5411f37d3be24afe6dff44d54a
Erik Langemalm <erik.langemalm@gmail.com>
2026-02-08 21:56:28 +0100

    Added libraries and tested running the flask app
    added and ran the flask app, haven't gotten to testing the connectivity for the database yet

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
 from flask import request
+from flask import Flask
+import mysql.connector as connector
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    
+    return "<p>Hello, World!</p>"
