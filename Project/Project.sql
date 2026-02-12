1966807490362fb3f517b8c4fb2ed4d43b301811
Max Bacharach <madw23@student.bth.se>
2026-02-12 15:29:33 +0100

    Elementen för Category, Subcategor och Product fix

--- Project/Project.sql
+++ Project/Project.sql
@@ -99,7 +99,13 @@ insert into Subcategory (name, category_id) values
 ('Cider & Blanddrycker', 5),
 ('Glögg & Andra Juldrycker', 5),
 ('Drinkar & Cocktail', 5),
-('Must', 5);
+('Must', 5),
+('Rött Vin', 5),
+('Vitt Vin', 5),
+('Rosé Vin', 5),
+('Aperitif & Bitter', 5),
+('Avec', 5),
+('Snaps', 5);
 
 insert into Product (name, country, price, volume_ml, alcohol_percent, category_id, subcategory_id) values
 -- Vin
@@ -240,4 +246,151 @@ insert into Product (name, country, price, volume_ml, alcohol_percent, category_
 ('Meukow Deluxe', 'Frankrike', 349, 700, 40.0, 3, 22),
 ('Braastad', 'Frankrike', 399, 700, 40.0, 3, 22),
 ('Hennessy VS', 'Frankrike', 499, 700, 40.0, 3, 22),
--- Rom % Lagrad Sockerrörssprit
+-- Rom & Lagrad Sockerrörssprit
+('Planetery', 'Barbados', 309, 700, 40.0, 3, 23),
+('Port Louiis', 'Mauritius', 119, 200, 40.0, 3, 23),
+('Havana Club', 'Kuba', 262, 700, 37.5, 3, 23),
+('Ron Zacapa', 'Guatemala', 599, 700, 40.0, 3, 23),
+('Don Papa', 'Filippinerna', 499, 700, 40.0, 3, 23),
+-- Vodka & Okryddat Brännvin
+('Absolute Vodka', 'Sverige', 249, 700, 40.0, 3, 24),
+('Dworek Vodka', 'Polen', 120, 350, 37.5, 3, 24),
+('Koskenkorva Vodka', 'Finland', 131, 350, 37.5, 3, 24),
+('Chum Churum', 'Sydkorea', 133, 350, 16.5, 3, 24),
+('Nemiroff', 'Ukraina'm 239, 700, 37.5, 3, 24),
+-- Grappa & Marc
+('Villa Isa', 'Italien', 299, 500, 42.0, 3, 25),
+('Montresor', 'Italien', 319, 700, 40.0, 3, 25),
+('Rovero', 'Italien', 295, 500, 42.0, 3, 25),
+('Marc de Bourgogne', 'Frankrike', 399, 700, 40.0, 3, 25),
+-- Tequila & Mezcal
+('Sierra', 'Mexiko', 229, 500, 38.0, 3, 26),
+('Tosco', 'Mexiko', 139, 200, 40.0, 3, 26),
+('1800 Añjeo', 'Mexiko', 459, 700, 38.0, 3, 26),
+('Koch', 'Mexiko', 399, 700, 40.0, 3, 26),
+-- Smaksatt Sprit
+('Captain Morgan', 'Jamaica', 261, 700, 35.0, 3, 27),
+('Bacardi Razz', 'Italien', 259, 700, 27.0, 3, 27),
+('Absloute Vodka, Citron', 'Sverige', 264, 700, 40.0, 3, 27),
+('Explorer Vodka, Röda Bär', 'Finland', 137, 350, 37.5, 3, 27),
+('Absolute Vodka, Vanilia', 'Sverige', 264, 700, 38.0, 3, 27),
+('Commander Jones', 'Nederländerna', 149, 350, 35.0, 3, 27),
+('Bumbu', 'Barbados', 449, 700, 40.0, 3, 27),
+('Absolute Vodka, Pear', 'Sverige', 264, 700, 38.0, 3, 27),
+('Chum Churum, Peach', 'Sydkorea', 134, 350, 12.0, 3,  27),
+('The Pirate Bay, Spiced', 'Dominikanska Republiken', 219, 700, 35.0, 3, 27),
+-- Armagnac & Brandy
+('Brandy Catedrático', 'Spanien', 95, 200, 36.0, 3, 28),
+('St-Rémy', 'Frankrike', 259, 700, 36.0, 3, 28),
+('Armagnac XO', 'Frankrike', 479, 700, 40.5, 3, 28),
+('Tegnér & Son, Brandy', 'Frankrike', 169, 350, 38.0, 3, 28),
+-- Drinkar & Cocktails
+('Tapdance', 'Sverige', 159, 700, 15.0, 3, 29),
+('Bacardi, Mojito Classic', 'Internationellt Märke', 155, 700, 14.9, 3, 29),
+('Tr3 Apor, Päronglass Daiquiri', 'Sverige', 139, 700, 15.0, 3, 29),
+('Hey Bae, Strawberry Daiquiri', 'Sverige', 169, 700, 15.0, 3, 29),
+-- Bitter
+('Gammel Dansk', 'Internationellt Märke', 318, 700, 38.0, 3, 30),
+('Fernet-Branca', 'Italien', 254, 500, 39.0, 3, 30),
+('Angostura', 'Trinidad och Tobago', 249, 200, 44.7, 3, 30),
+-- Calvados
+('Anée Fine Calvados Cuvée des Capucins', 'Frankrike', 236, 500, 40.0, 3, 31),
+('Boular Calvados Grand Solage', 'Frankrike', 359, 700, 40.0, 3, 31),
+('Château du Breuil', 'Frankrike', 369, 700, 40.0, 3, 31),
+('Kiviks', 'Frankrike', 299, 500, 40.0, 3, 31),
+-- Frukt & Druvsprit
+('Eau de vie de Cidre', 'Frankrike', 399, 700, 40.0, 3, 32),
+('Skånsk Åker av Dos Destiladores', 'Sverige', 299, 500, 41.5, 3, 32),
+('Ljungbyholms', 'Sverige', 599, 500, 40.0, 3, 32),
+('Kulla-D', 'Sverige', 538, 500, 40.0, 3, 32),
+('Pomme de Vie', 'Sverige', 328, 350, 40.0, 3, 32),
+-- Anissprit
+('Pastis Nordique', 'Sverige', 129, 200, 40.0, 3, 33),
+('Yeni Raki', 'Turkiet', 379, 700, 45.0, 3, 33),
+('Pernod', 'Frankrike', 286, 700, 40.0, 3, 33),
+('137:ans Arak', 'Sverige', 269, 500, 46.9, 3, 33),
+-- Punsch
+('Carlshamns Flaggpunsch', 'Finland', 199, 500, 26.0, 3, 34),
+('Cederlunds', 'Finland', 199, 700, 26.0, 3, 34),
+('Per Anderssons', 'Sverige', 189, 350, 22.0, 3, 34),
+('Bestes Punsch', 'Sverige', 159, 350, 26.0, 3, 34),
+
+-- Cider & Blanddrycker
+-- Cider
+('Briska Hallon', 'Sverige', 17.90, 330, 4.5, 4, 35),
+('Somersby, Pear Cider', 'Internationellt Märke', 15.50, 330, 4.5, 4, 35),
+('Kopparberg Pear Cider', 'Sverige', 20.90, 500, 7, 4, 35),
+('Rekordling Skogsbär', 'Sverige', 500, 7, 4, 35),
+('POMPOM Rosé', 'Sverige', 16.90, 330, 5.9, 4, 35),
+('Strongbow', 'Storbritannien', 19.90, 330, 5, 4, 35),
+('Liquid Industries, The Pina Colada Project', 'Sverige', 16.90, 330, 4.5, 4, 35),
+('Briska, Passionfrukt & Persika', 'Sverige', 15.90, 330, 4.5, 4, 35),
+('Liquid Industires, The Sour Project Grapefrukt, Lindblomma och Grönt Te', 'Sverige', 16.90, 330, 4.5, 4, 35),
+('Rekordling Päron', 'Sverige', 14.90, 330, 4.5, 4, 35),
+('Pistonhead, Hallon och Svarta Vinbär', 'Sverige', 14.90, 330, 4.5, 4, 35),
+('Pistonhead, Hard Apple', 'Sverige', 16.90, 330, 6, 4, 35),
+('Briska, Mango', 'Sverige', 15.90, 330, 4.5, 4, 35),
+('Herrljunga, Smultron & Jordgubb Cider', 'Sverige', 15.40, 250, 4.5, 4, 35),
+('Cidre Biologique', 'Frankrike', 66, 750, 4.5, 4, 35),
+-- Blanddryck
+('Lauder´s Whisky & Cola', 'Sverige', 23.90, 330, 4.5, 4, 36),
+('Tapdance, Espresso Martini', 'Sverige', 24.90, 200, 5, 4, 36),
+('POMPOM, Tropical', 'Sverige', 16.90, 330, 4.5, 4, 36),
+('Liquid Industires, The Bellini Project', 'Sverige', 16.90, 330, 4.5, 4, 36),
+('Doctor Cane, Mango Daiquiri', 'Sverige', 24.20, 250, 4, 4, 36),
+('Original Long Drink, Grapefruit', 'Finland', 25.90, 330, 5.5, 4, 36),
+('Smirnoff Ice', 'Internationellt Märke', 22.90, 275, 4, 4, 36),
+('FORS Cocktails, Tom Collins Organic', 'Sverige', 25.90, 330, 5, 4, 36),
+('Ginger Joe', 'Internationellt Märke', 21.90, 330, 4, 4, 36),
+('Locla Hard Seltzer', 'Sverige', 21.90, 330, 4.5, 4, 36),
+
+-- Alkoholfritt
+-- Öl
+('Oppigårds Winter Ale', 'Sverige', 11.90, 330, 0, 5, 37),
+('Omnipollo', 'Belgien', 21.90, 330, 0, 5, 37),
+('Vega, Easy IPA', 'Sverige', 19.90, 330, 0, 5, 37),
+('Funky Fluid Ice Cream Sour', 'Polen', 39.80, 500, 0, 5, 37),
+-- Mousserande
+('Cognato Sparkling', 'Sydafrika', 79, 750, 0, 5, 38),
+('Leitz', 'Tyskland', 32, 250, 0, 5, 38),
+('Soolong Sparkling Tea', 'Tyskland', 149, 750, 0, 5, 38),
+-- Cider & Blanddryck
+('Apple Ice Tea Ekologisk', 'Sverige', 24.90, 250, 0, 5, 39),
+('Björkad Cider', 'Sverige', 26.90, 250, 0, 5, 39),
+('Galipette, Rosé', 'Frankrike', 19.90, 330, 5, 39),
+('Kimino', 'Japan', 28.90, 250, 0, 5, 39),
+-- Glögg & Andra Juldrycker
+('Stockholms Bränneri, Winter Nogroni', 'Sverige', 79, 500, 0, 5, 40),
+('Dufvenkrooks, Röd Glögg', 'Sverige', 45, 1000, 0, 5, 40),
+('Duvenkrooks Saffrans Glögg', 'Sverige', 39, 375, 0, 5, 40),
+('Blossa Sparkling & Spice, Classic Red', 'Tyskland', 61, 750, 0, 5, 40),
+-- Drinkar & Martini
+('ISH Lime Daiquiri', 'Österrike', 21.50, 250, 0, 5, 41),
+('Stockholms Bränneri Spicy Pink Paloma', 'Sverige', 22.90, 250, 0, 5, 41),
+('ISH, Espresso Cocktail Alcohol Free', 'Danmark', 19.90, 250, 0, 5, 41),
+('Americano', 'Sverige', 22.80, 250, 0, 5, 41),
+-- Must
+('Gravensteiner', 'Sverige', 69, 500, 0, 5, 42),
+('Grönsöömust', 'Sverige', 74, 750, 0, 5, 42),
+-- Rött Vin
+('The Flourishing', 'Belgien', 84, 750, 0, 5, 43),
+('Sangre de Toro', 'Spanien', 42, 375, 0, 5, 43),
+('Domaine de la Prade', 'Frankrike', 89, 750, 0, 5, 43),
+-- Vitt Vin
+('Edenvale', 'Australien', 99, 750, 0, 5, 44),
+('Riesling Returns', 'Tyskland', 79, 750, 0, 5, 44),
+('Edenvale Verdejo-Sauvignon Blanc', 'Spanien', 89, 750, 0, 5, 44),
+-- Rosé Vin
+('Periquita Rosé', 'Portugal', 65, 750, 0, 5, 45),
+('Leitz', 'Tyskland', 74, 750, 0, 5, 45),
+-- Aperitif & Bitter
+('Hvonn Aperitif', 'Sverige', 169, 500, 0, 5, 46),
+('Mionetto Aperitivo', 'Polen', 149, 500, 0, 5, 46),
+('Stockholms Bränneri Apéro Soda', 'Sverige', 21.80, 250, 0, 5, 46),
+-- Avec
+('Glen Dochus, Highland Blend Alcohol Free', 'Storbritannien', 179, 700, 0, 5, 47),
+('Lyre´s American Malt, Non Alcoholic', 'Nerderländerna', 189, 700, 0, 5, 47),
+-- Snaps
+('Skåne, Alkoholfri Snaps', 'Sverige', 69, 350, 0, 5, 48),
+('O.P. Anderson, Alkoholfri Snaps Mixpack', 'Sverige', 109, 300, 0, 5, 48);
+
